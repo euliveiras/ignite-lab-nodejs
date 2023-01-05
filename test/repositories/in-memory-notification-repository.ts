b8b17d46d7d32936ca4c4aba193c4ccfd0cb792e
euliveiras <matheus.skm@hotmail.com>
2023-01-05 12:32:48 -0300

    created tests

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -2,10 +2,29 @@ import { Notification } from '@application/entities/notification';
 import { NotificationRepository } from '@application/repositories/notification-repository';
 
 export class InMemoryNotificationRepository implements NotificationRepository {
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
     this.notifications.push(notification);
-    console.log(notification);
   }
 }
